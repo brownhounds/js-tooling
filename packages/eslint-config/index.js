7c56ba0bb87bf746afb22a51858cb00043b7618a
brownhounds <brownhounds739@gmail.com>
2024-03-30 11:01:00 +0000

    Chores: Update eslint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,7 +2,7 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'prettier'],
+    plugins: ['@typescript-eslint', 'prettier', 'no-null'],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
@@ -17,5 +17,18 @@ module.exports = {
         '@typescript-eslint/no-unused-vars': 'error',
         '@typescript-eslint/explicit-function-return-type': 'error',
         '@typescript-eslint/no-floating-promises': 'error',
+        '@typescript-eslint/consistent-type-imports': 'error',
+        '@typescript-eslint/consistent-type-exports': 'error',
+        '@typescript-eslint/consistent-type-assertions': 'error',
+        '@typescript-eslint/consistent-return': 'error',
+        'no-null/no-null': 'error',
     },
+    overrides: [
+        {
+            files: ['*.ts'],
+            parserOptions: {
+                project: ['./tsconfig.json'],
+            },
+        },
+    ],
 };
